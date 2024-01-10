6267699ff40678d5a1ef4ef479c77bca83a4b76d
Andryyyshka <andrey_xoxlov_2000@mail.ru>
2024-01-10 03:29:07 +0300

    Решены ошибки связанные с Доступности слоев И добавлены TextBox для вывода данных

--- Noted/ChildForm.Designer.cs
+++ Noted/ChildForm.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.contentTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -41,21 +43,40 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(31, 108);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(192, 22);
+            this.titleTextBox.TabIndex = 1;
+            // 
+            // contentTextBox
+            // 
+            this.contentTextBox.Location = new System.Drawing.Point(31, 182);
+            this.contentTextBox.Name = "contentTextBox";
+            this.contentTextBox.Size = new System.Drawing.Size(100, 22);
+            this.contentTextBox.TabIndex = 2;
+            // 
             // ChildForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(352, 323);
+            this.Controls.Add(this.contentTextBox);
+            this.Controls.Add(this.titleTextBox);
             this.Controls.Add(this.button1);
             this.Name = "ChildForm";
             this.Text = "ChildForm";
             this.Load += new System.EventHandler(this.ChildForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox contentTextBox;
     }
 }
--- Noted/Note.cs
+++ Noted/Note.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Noted
 {
-    internal class Note
+     public class Note
     {
         public int NoteId { get; set; }
         public int WindowId { get; set; }
